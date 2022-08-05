62d337255fbf3159bebc8c850a6da7b94926438d
Kanhav Gupta <kanhav@Kanhav-Tech.local>
2022-08-05 17:22:28 +0530

    something

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Carousel1 from './carousel1';
+import Gallery from './gallery';
+import JoinType from './joinType';
+import NavBar from './navBar';
+import NavBar2 from './navBar2';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar/>
+      <NavBar2/>
+      <Carousel1/>
+      {/* <JoinType/> */}
+      {/* <Gallery/> */}
     </div>
   );
 }
